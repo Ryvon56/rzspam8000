3d0aae89ee62a6a084151c70169eb9b80eca2acf
Ryvon56 <52981546+Ryvon56@users.noreply.github.com>
2019-08-11 16:22:57 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("610093175130030080")
+setInterval(function() {
+channel.send(`انا هنا للترفيه عن نفسي فقط فلا يظن عقلك المصدي أنني اطمح للتقرب منك`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
